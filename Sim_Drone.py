29f3f1b9a20b2ed3273e4759d42a718217edee73
manavgandhi17 <manavgandhi17@gmail.com>
2020-12-01 17:54:46 -0500

    Update Sim_Drone.py

--- Sim_Drone.py
+++ Sim_Drone.py
@@ -1,4 +1,6 @@
 #Simple simulated drone initiation.
+#Note Dronekit simulation launches ArduCopter 3.3 for testing - which is a very old version. Since we only use this to test interop operations - the firmware version doesn't matter for now.
+#However, if there were to a large overhaul to the platform in the future (in terms of comms) - a different method of simulation may be required.
 
 import dronekit_sitl
 from time import sleep
