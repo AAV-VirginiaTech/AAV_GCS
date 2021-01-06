9e1956580878a9fa5a0fd7f24d3a224d2129d7be
manavgandhi17 <manavgandhi17@gmail.com>
2021-01-05 23:17:18 -0500

    Update modify.bat

--- modify.bat
+++ modify.bat
@@ -1,3 +1,5 @@
+#This document pulls the latest interop server, performs any modifications based on the dockerfile (if needed), and pushes the new image to AAV's docker account.
+
 #!/bin/bash
 
 docker build -t aavvt/interop:latest .
