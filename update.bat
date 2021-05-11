84afa8cdfe4ef74965695a94b103f1649c9fde65
manavgandhi17 <manavgandhi17@gmail.com>
2021-05-11 13:07:55 -0400

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,6 +1,6 @@
-#This document pulls the latest AUVSI SUAS interop image, performs any modifications based on the dockerfile (if needed), and pushes the new image to AAV's docker account.
+REM This document builds the latest AUVSI SUAS interop image, performs any modifications based on the dockerfile (if needed), and pushes the new image to AAV's docker account.
 
 #!/bin/bash
 
-docker build -t auvsisuas/interop-client:latest .
+docker build -t aavvt/interop:latest .
 docker push aavvt/interop:latest
