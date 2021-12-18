f42faeb5d4f32cef6f0894bc50ac155be53e32a2
manavgandhi17 <manavgandhi17@gmail.com>
2021-12-18 18:51:12 -0500

    Update and rename interop_to_files.py to mission_to_files.py

--- mission_to_files.py
+++ mission_to_files.py
@@ -1,7 +1,7 @@
 #Base file for taking interop data output and converting to mission planner files. Multiple files for the UGV and UAV are outputted.
 import math
 import json
-mission = input("Paste mission output:")
+mission = open("interop_mission.txt", "r").read()
 mission = json.loads(mission)
 template = '{0:d}\t{1:d}\t{2:d}\t{3:d}\t{4:.8f}\t{5:.8f}\t{6:.8f}\t{7:.8f}\t{8:.8f}\t{9:.8f}\t{10:.8f}\t{11:d}\n'
 # ------------------------------------------------------------------------------------------------------------------
