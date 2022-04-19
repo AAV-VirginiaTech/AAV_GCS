335b4044275619bded0c8b85c0638adb6b386437
Manav Gandhi <manavgandhi17@gmail.com>
2022-04-19 00:39:53 -0400

    Delete update.bat

--- update.bat
+++
@@ -1,6 +0,0 @@
-REM This document builds the latest AUVSI SUAS interop image, performs any modifications based on the dockerfile (if needed), and pushes the new image to AAV's docker account.
-
-#!/bin/bash
-
-docker build -t aavvt/interop:latest .
-docker push aavvt/interop:latest
